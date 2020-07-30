a4f527c161fd7089fbbf60bb9147a55563a0d375
jlassign@hpe.com <jlassign@hpe.com>
2020-07-30 19:37:46 +0200

    Finalyzing script without thread

--- rebalancing_agents_load.py
+++ rebalancing_agents_load.py
@@ -3,6 +3,7 @@
 import openstack, inspect, math
 import sys, os, re, threading, queue, argparse
 from optparse import OptionParser
+from operator import itemgetter
 
 #### Variable Globale
 adhcp_nets = {}
@@ -14,11 +15,12 @@ hash_al3 = {}
 hash_router = {}
 
 average_l3_by_ag = 0
-average_net_by_ag  0
+average_net_by_ag = 0
 
 stop_file="/tmp/rebalancing_agents_load.stop"
+dryrun = False
 
-## Calcul le nombre de reseau ou routeur moyen
+## Calcul le nombre de reseau ou routeur moyen par agent arrondie au chiffre superieur
 def average(hash_agent):
     return math.ceil(sum(list(map((lambda x :len(hash_agent[x])), hash_agent)))/len(hash_agent))
 
@@ -54,7 +56,7 @@ def init_data_dhcp(auth):
         dhcp_agents = list(auth.network.agents(agent_type="DHCP agent"))
         nb_gl_networks = 0
         for dhcp_agent in dhcp_agents:
-            if "neut-m" not in dhcp_agent.host or dhcp_agent.is_active is False:
+            if "neut-m" not in dhcp_agent.host or dhcp_agent.is_alive is False:
                 continue
             hash_adhcp[dhcp_agent.id] = dhcp_agent
             adhcp_nets[dhcp_agent.id] = {}
@@ -77,7 +79,7 @@ def init_data_l3(auth):
         l3_agents = list(auth.network.agents(agent_type="L3 agent"))
         nb_gl_routers = 0
         for l3_agent in l3_agents:
-            if "neut-m" not in l3_agent.host or l3_agent.is_active is False:
+            if "neut-m" not in l3_agent.host or l3_agent.is_alive is False:
                 continue
             hash_al3[l3_agent.id] = l3_agent
             al3_routers[l3_agent.id] = {}
@@ -106,7 +108,7 @@ def get_agent_network(agent, asc_agent, agent_net, average_network):
         return -1
 
 ## Get New agent possedant le moins de routeur 
-def get_agent_router(agent, asc_agent, average_router):
+def get_agent_router(agent, asc_agent, average_router):    
     try:
         for ag in asc_agent:
             if ag[1] > average_network:
@@ -121,25 +123,30 @@ def get_agent_router(agent, asc_agent, average_router):
     
 ## pour un agent on deplace les reseaux sur d'autres agent pour retomber en dessous de la moyenne
 def adding_removing_network(auth, agent, average_network):
+    global adhcp_nets, hash_net, dryrun
     try:
         asc_agent = pair_list_nb_by_id_asc(adhcp_nets)
 
         while len(adhcp_nets[agent]) <= average_network:
-            if os.path.exist(stop_file):
-                print("Find file %s : stopping" %s stop_file)
+            if os.path.exists(stop_file):
+                print("Find file %s : stopping" % stop_file)
                 return -2            
             keys = adhcp_nets[agent].keys()
             if len(keys) == 0:
                 return  -1
             net = keys[0]
             agent_net =  get_agent_id_form_network(net)
+            if len(agent_net) != 2:
+                print("Warning !!! Net %s On only one agent : %s" % (net, " ".join(agent_net)))
             new_agent = get_agent_network(agent, asc_agent, agent_net, average_network)
             if new_agent is None:                
                 return -1
             print("Adding Net %s to Agent DHCP : %s" % (net, new_agent))
-            resp1 = auth.network.add_dhcp_agent_to_network(new_agent, hash_net[net])
+            if not dryrun:
+                resp1 = auth.network.add_dhcp_agent_to_network(new_agent, hash_net[net])
             print("Removing Net %s to Agent DHCP : %s" % (net, agent))
-            resp2 = auth.network.remove_dhcp_agent_from_network(agent, hash_net[net])
+            if not dryrun:
+                resp2 = auth.network.remove_dhcp_agent_from_network(agent, hash_net[net])
             adhcp_nets[new_agent][net] = adhcp_nets[agent][net]
             del(adhcp_nets[agent][net])
             asc_agent = pair_list_nb_by_id_asc(adhcp_nets)
@@ -151,12 +158,13 @@ def adding_removing_network(auth, agent, average_network):
 
 ## pour un agent on deplace les routeurs sur d'autres agent pour retomber en dessous de la moyenne
 def adding_removing_router(auth, agent, average_router):
+    global al3_routers, hash_router, dryrun
     try:
         asc_agent = pair_list_nb_by_id_asc(al3_routers)
 
         while len(al3_routers[agent]) <= average_router:
-            if os.path.exist(stop_file):
-                print("Find file %s : stopping" %s stop_file)
+            if os.path.exists(stop_file):
+                print("Find file %s : stopping" % stop_file)
                 return -2            
             keys = al3_routers[agent].keys()
             if len(keys) == 0:
@@ -165,10 +173,12 @@ def adding_removing_router(auth, agent, average_router):
             new_agent = get_agent_router(agent, asc_agent)
             if new_agent is None:                
                 return -1
-            print("Adding Router %s to Agent L3 : %s" % (net, new_agent))            
-            resp1 = auth.network.add_router_to_agent(new_agent, hash_router[router])
-            print("Removing Router %s to Agent L3 : %s" % (net, agent))            
-            resp2 = auth.network.remove_router_from_agent(agent, hash_router[router])
+            print("Adding Router %s to Agent L3 : %s" % (net, new_agent))
+            if not dryrun:
+                resp1 = auth.network.add_router_to_agent(new_agent, hash_router[router])
+            print("Removing Router %s to Agent L3 : %s" % (net, agent))
+            if not dryrun:
+                resp2 = auth.network.remove_router_from_agent(agent, hash_router[router])
             al3_routers[new_agent][router] = al3_routers[agent][souter]
             del(al3_routers[agent][router])
             asc_agent = pair_list_nb_by_id_asc(al3_routers)
@@ -180,12 +190,13 @@ def adding_removing_router(auth, agent, average_router):
 
 ## On balance les agents les plus charges en reseau 
 def balancing_network(auth):
+    global adhcp_nets
     try:
         average_network = average(adhcp_nets)
         dsc_agent = pair_list_nb_by_id_dsc(adhcp_nets)
         while [ 1 ]:
-            if os.path.exist(stop_file):
-                print("Find file %s : stopping" %s stop_file)
+            if os.path.exists(stop_file):
+                print("Find file %s : stopping" % stop_file)
                 return -2
             if dsc_agent[0][1] <= average_network:
                 break
@@ -201,12 +212,13 @@ def balancing_network(auth):
 
 ## On balance les agents les plus charges en routeur
 def balancing_router(auth):
+    global al3_routers
     try:
         average_router = average(al3_routers)
         dsc_agent = pair_list_nb_by_id_dsc(al3_routers)
         while [ 1 ]:
-            if os.path.exist(stop_file):
-                print("Find file %s : stopping" %s stop_file)
+            if os.path.exists(stop_file):
+                print("Find file %s : stopping" % stop_file)
                 return -2
             if dsc_agent[0][1] <= average_network:
                 break
@@ -222,6 +234,7 @@ def balancing_router(auth):
 
 ## Retourne l'UUID de l'agent DHCP pour le noeud
 def get_agent_dhcp_from_node(node):
+    global hash_adhcp
     try:
         for agent in hash_adhcp.keys():
             if hash_adhcp[agent].host == node:
@@ -233,6 +246,7 @@ def get_agent_dhcp_from_node(node):
     
 ## Retourne l'UUID de l'agent L3 pour le noeud
 def get_agent_l3_from_node(node):
+    global hash_al3
     try:
         for agent in hash_al3.keys():
             if hash_al3[agent].host == node:
@@ -244,6 +258,7 @@ def get_agent_l3_from_node(node):
 
 ## Evacue les reseaux sur les autres agents
 def evacuate_network(auth, node):
+    global adhcp_nets, hash_net, dryrun
     try:
         agent = get_agent_dhcp_from_node(node)
         if agent is None:
@@ -254,17 +269,19 @@ def evacuate_network(auth, node):
         average_network = average(adhcp_nets)
 
         for net in data_agent.keys():
-            if os.path.exist(stop_file):
-                print("Find file %s : stopping" %s stop_file)
+            if os.path.exists(stop_file):
+                print("Find file %s : stopping" % stop_file)
                 return -2            
             agent_net =  get_agent_id_form_network(net)
             new_agent = get_agent_network(agent, asc_agent, agent_net, average_network)
             if new_agent is None:                
                 return -1
             print("Adding Net %s to Agent DHCP : %s" % (net, new_agent))
-            resp1 = auth.network.add_dhcp_agent_to_network(new_agent, hash_net[net])
+            if dryrun:
+                resp1 = auth.network.add_dhcp_agent_to_network(new_agent, hash_net[net])
             print("Removing Net %s to Agent DHCP : %s" % (net, agent))
-            resp2 = auth.network.remove_dhcp_agent_from_network(agent, hash_net[net])
+            if dryrun:
+                resp2 = auth.network.remove_dhcp_agent_from_network(agent, hash_net[net])
             adhcp_nets[new_agent][net] = data_agent[net]
             asc_agent = pair_list_nb_by_id_asc(adhcp_nets)
         return 0
@@ -274,6 +291,7 @@ def evacuate_network(auth, node):
 
 ## Evacue les routeurs sur les autres agents
 def evacuate_router(auth, node):
+    global al3_routers, hash_router, dryrun
     try:
         agent = get_agent_router_from_node(node)
         if agent is None:
@@ -285,17 +303,18 @@ def evacuate_router(auth, node):
         asc_agent = pair_list_nb_by_id_asc(al3_routers)
             
         for router in data_agent.keys():
-            if os.path.exist(stop_file):
-                print("Find file %s : stopping" %s stop_file)
+            if os.path.exists(stop_file):
+                print("Find file %s : stopping" % stop_file)
                 return -2            
-            agent_router =  get_agent_id_form_router(router)
             new_agent = get_agent_router(agent, asc_agent, average_router)
             if new_agent is None:                
                 return -1
-            print("Adding Router %s to Agent L3 : %s" % (net, new_agent))            
-            resp1 = auth.network.add_router_to_agent(new_agent, hash_net[net])
-            print("Removing Router %s to Agent L3 : %s" % (net, agent))            
-            resp2 = auth.network.remove_router_from_agent(agent, hash_net[net])
+            print("Adding Router %s to Agent L3 : %s" % (net, new_agent))
+            if dryrun:
+                resp1 = auth.network.add_router_to_agent(new_agent, hash_router[router])
+            print("Removing Router %s to Agent L3 : %s" % (net, agent))
+            if dryrun:
+                resp2 = auth.network.remove_router_from_agent(agent, hash_router[router])
             al3_routers[new_agent][net] = data_agent[net]
             asc_agent = pair_list_nb_by_id_asc(al3_routers)
         return 0
@@ -305,18 +324,19 @@ def evacuate_router(auth, node):
 
 ## Bilan du placements des reseaux et routeurs sur les agents
 def check_load():
-        try:
+    global adhcp_nets, al3_routers
+    try:
             print("# Network by agent:\n")
             asc_agent = pair_list_nb_by_id_asc(adhcp_nets)
 
             for ag in asc_agent:
                 print("Agent: %s # Network : %d" % (ag[0], ag[1]))
 
-            print("# Router by agent:\n")
+            print("\n\n# Router by agent:\n")
             asc_agent = pair_list_nb_by_id_asc(al3_routers)
 
             for ag in asc_agent:
-                print("Agent: %s # Network : %d" % (ag[0], ag[1]))
+                print("Agent: %s # Router : %d" % (ag[0], ag[1]))
                 
     except Exception as e:
         print("Fatal Error in %s Err : %s" % (inspect.stack()[0][3], e))
@@ -339,15 +359,38 @@ def main():
                             default=None, choices = [ "balancing", "evacuate"],\
                             help="action to perform choices balancing or evacuate")
 
+        ### Dry run
+        parser.add_argument('--dryrun', dest='dryrun', action='store_true', help="Dry Run option")
+
+        ### Stat Only
+        parser.add_argument('--statonly', dest='stat', action='store_true', help="Stat Only option")
+                      
         args = parser.parse_args()
 
         ### recuperation des paramètres
         node = args.node
         action = args.action
+        dryrun = args.dryrun
+        statonly = args.stat
 
         ### Check stop file
-        if os.path.exist(stop_file):
-            print("Find file %s : stopping" %s stop_file)
+        if os.path.exists(stop_file):
+            print("Find file %s : stopping" % stop_file)
+
+        ### Initialisation de la connection
+        auth = init_conn()
+
+        ### Etat initial Agent DHCP
+        init_data_dhcp(auth)
+
+        ### Etat initial Agent L3        
+        init_data_l3(auth)
+
+        ### Check de l'équilibrage au demarrage
+        check_load()
+
+        if statonly:
+            sys.exit(0)
 
         ### verfication des arguments
         if action not in ["balancing", "evacuate"]:
@@ -361,18 +404,6 @@ def main():
         if action == "evacuate" and "neut-m" in node:
             print("Syntaxe is Wrong !!! action evacuate Wrong node name expecting name containing neut-m")
             sys.exit(1)
-
-        ### Initialisation de la connection
-        auth = init_conn()
-
-        ### Etat initial Agent DHCP
-        init_data_dhcp(auth)
-
-        ### Etat initial Agent L3        
-        init_data_l3(auth)
-
-        ### Check de l'équilibrage au demarrage
-        check_load()
         
         ### Balancing des reseaux et routeurs
         if action == "balancing":
